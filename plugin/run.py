9a68f80f0eaf8f389a068a99dc4e95c2a92574de
miyakogi <miyako.dev@gmail.com>
2016-01-29 22:34:47 +0900

    Pass through options in python

--- plugin/run.py
+++ plugin/run.py
@@ -7,6 +7,10 @@ import subprocess
 
 if __name__ == '__main__':
     script = path.join(path.dirname(__file__), 'livemark.py')
-    p = subprocess.Popen([sys.executable, script])
-    print(p.pid)
+    args = [sys.executable, script]
+    for arg in sys.argv:
+        if arg.startswith('-'):
+            args.append(arg)
+    p = subprocess.Popen(args)
+    print(p.pid, end='')
     sys.exit()
